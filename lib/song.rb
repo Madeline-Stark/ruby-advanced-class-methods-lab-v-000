480d4642362130eaa316963afc15ebf1d75f0e7b
Madeline Stark <madeline.stark1@gmail.com>
2018-04-11 18:50:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,9 +50,8 @@ class Song
   def self.new_from_filename(string)
     song = self.find_or_create_by_name(string)
     data = string.split(/[-,.]/)
-    data.delete(/[\s]/)
-    artist_name = data[0]
-    name = data[1]
+    artist_name = data[0].delete(/[\s]/)
+    name = data[1].delete(/[\s]/)
     song.artist_name = artist_name
     song.name = name
     song
