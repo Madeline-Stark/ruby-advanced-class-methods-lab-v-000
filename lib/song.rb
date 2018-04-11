86d58876878faf3f85d175a425781f092367872f
Madeline Stark <madeline.stark1@gmail.com>
2018-04-11 18:56:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,7 @@ class Song
   def self.create_by_name(name)
     #song = self.new
     #song.name = name
-    song = self.new_by_name
+    song = self.new_by_name(name)
     @@all << song
     song
   end
